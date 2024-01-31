188f698bd24dcc7c07ca359df9a6df03896d2813
Hady mohamed <hadykamel3@gmail.com>
2024-01-31 20:49:15 +0200

    complete package

---
+++ lib/paint_package.dart
@@ -0,0 +1,5 @@
+library paint_package;
+
+import 'package:flutter/material.dart';
+
+const Color kCanvasColor = Color(0xfff2f3f7);
---
+++ lib/view/drawing_page.dart
@@ -0,0 +1,152 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart' hide Image;
+import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:paint_package/paint_package.dart';
+import 'package:paint_package/view/drawing_canvas/drawing_canvas.dart';
+import 'package:paint_package/view/drawing_canvas/models/drawing_mode.dart';
+import 'package:paint_package/view/drawing_canvas/models/sketch.dart';
+import 'package:paint_package/view/drawing_canvas/widgets/canvas_side_bar.dart';
+
+typedef OnSketch = Function(Sketch sketch);
+
+class DrawingPage extends StatefulHookWidget {
+  const DrawingPage({
+    Key? key,
+    required this.onSketch,
+    this.appBarTitle = "",
+  }) : super(key: key);
+  final OnSketch onSketch;
+  final String appBarTitle;
+  @override
+  createState() => _DrawingPageState();
+}
+
+class _DrawingPageState extends State<DrawingPage> {
+  @override
+  void initState() {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final selectedColor = useState(Colors.black);
+    final strokeSize = useState<double>(10);
+    final eraserSize = useState<double>(30);
+    final drawingMode = useState(DrawingMode.pencil);
+    final filled = useState<bool>(false);
+    final polygonSides = useState<int>(3);
+    final backgroundImage = useState<Image?>(null);
+
+    final canvasGlobalKey = GlobalKey();
+
+    ValueNotifier<Sketch?> currentSketch = useState(null);
+    ValueNotifier<List<Sketch>> allSketches = useState([]);
+
+    final animationController = useAnimationController(
+      duration: const Duration(milliseconds: 150),
+      initialValue: 1,
+    );
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: kCanvasColor,
+            width: double.maxFinite,
+            height: double.maxFinite,
+            child: DrawingCanvas(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              onSketch: widget.onSketch,
+              drawingMode: drawingMode,
+              selectedColor: selectedColor,
+              strokeSize: strokeSize,
+              eraserSize: eraserSize,
+              sideBarController: animationController,
+              currentSketch: currentSketch,
+              allSketches: allSketches,
+              canvasGlobalKey: canvasGlobalKey,
+              filled: filled,
+              polygonSides: polygonSides,
+              backgroundImage: backgroundImage,
+            ),
+          ),
+          Positioned(
+            top: kToolbarHeight + 10,
+            // left: -5,
+            child: SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(-1, 0),
+                end: Offset.zero,
+              ).animate(animationController),
+              child: CanvasSideBar(
+                drawingMode: drawingMode,
+                selectedColor: selectedColor,
+                strokeSize: strokeSize,
+                eraserSize: eraserSize,
+                currentSketch: currentSketch,
+                allSketches: allSketches,
+                canvasGlobalKey: canvasGlobalKey,
+                filled: filled,
+                polygonSides: polygonSides,
+                backgroundImage: backgroundImage,
+              ),
+            ),
+          ),
+          SafeArea(
+            child: _CustomAppBar(
+              animationController: animationController,
+              appBarTitle: widget.appBarTitle,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  final AnimationController animationController;
+  final String appBarTitle;
+  const _CustomAppBar(
+      {Key? key, required this.animationController, required this.appBarTitle})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        height: kToolbarHeight,
+        width: double.maxFinite,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                onPressed: () {
+                  if (animationController.value == 0) {
+                    animationController.forward();
+                  } else {
+                    animationController.reverse();
+                  }
+                },
+                icon: const Icon(Icons.menu),
+              ),
+              Text(
+                appBarTitle,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 19,
+                ),
+              ),
+              const SizedBox.shrink(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
